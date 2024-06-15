75dc344f28f51e90c840045ef0cf8bc81ff2a6e0
samc5 <samcowan1968@gmail.com>
2024-06-15 18:25:59 -0400

    cors change, hopefully works

--- api/rss/main.py
+++ api/rss/main.py
@@ -16,7 +16,11 @@ load_dotenv()
 secret_key = os.getenv("SECRET")
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/graphql": {"origins": "*"},
+    r"/login": {"origins": "*"},
+    r"/signup": {"origins": "*"}
+})
 app.app_context().push()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
