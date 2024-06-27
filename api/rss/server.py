b96d24e026ff25405658ab82e61571532d4a1038
samc5 <samcowan1968@gmail.com>
2024-06-26 23:43:49 -0400

    edited prints in server (which is now running locally) and deleted mongo and parser from main directory (I haven't used those files in months and it gets a little confusng)

--- api/rss/server.py
+++ api/rss/server.py
@@ -13,9 +13,9 @@ while(1):
     print("Looping around...")
     urls = mongo.aggregate(url_pipeline)
     feed_dicts = [parser.construct_feed_dict(i['url']) for i in urls]
-    count = 1
-    for i in feed_dicts:
-        if count % 50 == 0:
-            print(i)
-        count += 1
+    # count = 1
+    # for i in feed_dicts:
+    #     # if count % 50 == 0:
+    #     #     print(i)
+    #     # count += 1
     mongo.add_feeds(feed_dicts)
